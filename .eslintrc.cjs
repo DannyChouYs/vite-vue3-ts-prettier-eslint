5dabb89cb575b3bfc942cf6ab592b3014539ea68
JUMBO\dannychou <dannychou@jumbogames.com.tw>
2022-08-19 16:12:52 +0800

    fix: 修正eslint解析vue檔案報錯

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,15 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
-        node: true
+        node: true,
     },
     extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:@typescript-eslint/recommended'],
     overrides: [],
-    parser: '@typescript-eslint/parser',
+    parser: 'vue-eslint-parser',
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        parser: '@typescript-eslint/parser',
     },
     plugins: ['vue', '@typescript-eslint'],
     rules: {},
